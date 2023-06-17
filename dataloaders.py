9ac86deadad469dbee95ddcd03863a76dea33903
adityaprakash-work <adityaprakash.work@gmail.com>
2023-06-18 00:32:53 +0530

    Base1 defined

---
+++ dataloaders.py
@@ -0,0 +1,103 @@
+# ---DEPENDENCIES---------------------------------------------------------------
+import os
+
+import mne
+import numpy as np
+import scipy as sp
+import tftb
+import tensorflow as tf
+
+
+# ---BASES----------------------------------------------------------------------
+class Base1(tf.keras.utils.Sequence):
+    def __init__(
+        self,
+        final_data_shape,
+        batch_size,
+        directory=None,
+        classes=None,
+        shuffle=True,
+        validation_split=None,
+        validation_batch_size=None,
+        seed=None,
+        file_info=None,
+    ):
+        self.final_data_shape = final_data_shape
+        self.batch_size = batch_size
+        self.directory = directory
+        self.classes = classes
+        self.shuffle = shuffle
+        self.validation_split = validation_split
+        self.seed = 42 if seed is None else seed
+        self.rng = np.random.default_rng(self.seed)
+        if file_info is None:
+            if self.directory is None:
+                raise ValueError(
+                    "Directory must be specified if file_info is not given"
+                )
+            self.file_info = [
+                [
+                    os.path.join(self.directory, self.classes[c], f),
+                    c,
+                ]
+                for c in range(len(self.classes))
+                for f in os.listdir(os.path.join(self.directory, self.classes[c]))
+            ]
+        else:
+            self.file_info = file_info
+        self.file_info.sort(key=lambda x: x[0])
+        self.file_info = np.array(self.file_info)
+        self.rng.shuffle(self.file_info)
+
+        if self.validation_split is not None:
+            if validation_batch_size is None:
+                self.validation_batch_size = batch_size
+            else:
+                self.validation_batch_size = validation_batch_size
+            self.pri_file_info = self.rng.choice(
+                self.file_info,
+                size=int(len(self.file_info) * (1 - self.validation_split)),
+                replace=False,
+            )
+            self.aux_file_info = self.file_info[
+                ~np.isin(self.file_info, self.pri_file_info)
+            ]
+            self.aux_dgen = Base1(
+                data_shape=self.data_shape,
+                batch_size=self.validation_batch_size,
+                classes=self.classes,
+                shuffle=self.shuffle,
+                seed=self.seed,
+                file_info=self.aux_file_info,
+            )
+        else:
+            self.pri_file_info = self.file_info
+            self.aux_file_info = None
+            self.aux_dgen = None
+
+        self.on_epoch_end()
+
+    def __len__(self):
+        return len(self.pri_file_info) // self.batch_size
+
+    def __getitem__(self, index):
+        batch_info = self.pri_file_info[
+            index * self.batch_size : (index + 1) * self.batch_size
+        ]
+        Xb, Yb = self._batch_generator(batch_info)
+        return Xb, Yb
+
+    def on_epoch_end(self):
+        if self.shuffle:
+            np.random.shuffle(self.pri_file_info)
+
+    def _batch_generator(self, batch_info):
+        Xb = np.zeros((self.batch_size, *self.final_data_shape))
+        Yb = np.zeros((self.batch_size), dtype=int)
+        for i, (f, c) in enumerate(batch_info):
+            Xb[i], Yb[i] = self._processing_function(f), c
+        return Xb, Yb
+
+    # This method should be overriden by the child class
+    def _processing_function(self, x):
+        pass
